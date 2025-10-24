fbbc7dbafd436c95f2bdaff3f6d00b351bad5692
adriangarcia412-ui <adrian.garcia412@gmail.com>
2025-10-24 09:31:15 -0600

    Connect App.jsx in main.jsx
    Updated import statements and ReactDOM rendering syntax.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: '20px' }}>
-      <h1>SOC V3</h1>
-      <p>Nuevo Sistema de Observación de Comportamientos</p>
-    </div>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
